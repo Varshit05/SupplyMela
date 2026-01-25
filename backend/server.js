db3c3edf8aef97a9c95ff4d4525f7b8b372696ca
Varshit05 <khandelwalvarshit4705@gmail.com>
2026-01-25 19:17:53 +0530

    Fix api-5

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,10 @@ const app = express();
 app.use(cors({
 origin: [
 "http://localhost:5173",
-"https://supply-mela.vercel.app/"
+"https://supply-mela.vercel.app"
 ],
-credentials: true
+methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(express.json());
 
